49bd5037cc6017b348c8caa798bd9865d6b47997
Ahmad-mh1999 <ahmedmhmad021@gmail.com>
2024-06-11 18:49:52 +0300

    Navbar

---
+++ src/layout/Navbar/Navbar.jsx
@@ -0,0 +1,82 @@
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+import personImage from './../../assets/images/person-login.png'
+import { useState } from 'react'
+import { FaBars } from "react-icons/fa6";
+import Sidebar from '../Sidebar/Sidebar';
+function Navbar() {
+  const [activeItem, setActiveItem] = useState("home");
+  const [sideBarActive, setSideBarActive] = useState(false);
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+  const toggleDisplay = () => {
+    setSideBarActive(!sideBarActive);
+  };
+
+  return (
+    <>
+    <div className='navbar'>
+      <div className="container">
+        <div className="login-icon">
+          <img src={personImage} alt="person image" />
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link to='/recommendations'
+                className={`  ${activeItem === "recommendations" ? "active-nav" : ""
+                  }`}
+                onClick={()=>handleItemClick('recommendations')}>
+                التوصيات
+              </Link>
+            </li>
+            <li>
+              <Link to='blog'
+                className={`  ${activeItem === "blog" ? "active-nav" : ""
+                  }`}
+                onClick={()=>handleItemClick('blog')}>
+                المدونة
+              </Link>
+            </li>
+            <li>
+              <Link to='explore'
+                className={`  ${activeItem === "explore" ? "active-nav" : ""
+                  }`}
+                onClick={()=>handleItemClick('explore')}>
+                استكشف
+              </Link>
+            </li>
+            <li>
+              <Link to='about'
+                className={`  ${activeItem === "about" ? "active-nav" : ""
+                  }`}
+                onClick={()=>handleItemClick('about')}>
+
+                عن سوريا
+              </Link>
+            </li>
+            <li>
+              <Link to='/'
+                className={`  ${activeItem === "home" ? "active-nav" : ""
+                  }`}
+                onClick={()=>handleItemClick('home')}>
+                الرئيسية
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-brand">
+          <p><span>Visit</span> Syria</p>
+        </div>
+        <div className="bar-icon" onClick={toggleDisplay}>
+          <FaBars />
+        </div>
+      </div>
+    </div>
+    {sideBarActive && <Sidebar isActive={sideBarActive} toggleDisplay={toggleDisplay} />}
+    </>
+  )
+}
+
+export default Navbar
