7c4be194e489b57ba9be4a637478442c53f3886e
MoonesMezher <moonesmezher050@gmail.com>
2024-06-15 15:22:57 +0300

    photos slider + exploer layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes ,Route } from 'react-router-dom'
 import './App.css'
 import Footer from './layout/Footer/Footer'
-import HomePage from './pages/HomePage/HomePage'
-import ExplorePage from './pages/ExplorePage/ExplorePage'
+import HomePage from './pages/HomePage/Homepage'
+import ExplorePage from './shared/ExplorePage/ExplorePage'
 import AboutPage from './pages/AboutPage/AboutPage'
 import BlogPage from './pages/BlogPage/BlogPage'
 import BookPage from './pages/BookPage/BookPage'
 import Navbar from './layout/Navbar/Navbar'
-import Sidebar from './layout/Sidebar/Sidebar'
+// import Sidebar from './layout/Sidebar/Sidebar'
 import RecommendationsPage from './pages/RecommendationsPage/RecommendationsPage'
 
 
@@ -15,18 +15,16 @@ function App() {
 
   return (
     <>
-
-<Footer/>
-
-    <Navbar />
+    <Navbar/>
     <Routes>
       <Route path='/' element={<HomePage/>} />
-      <Route path='/explore' element={<ExplorePage />} />
+      {/* <Route path='/explore' element={<ExplorePage />} /> */}
       <Route path='/about' element={<AboutPage />} />
       <Route path='/blog' element={<BlogPage />} />
       <Route path='/booking' element={<BookPage />} />
       <Route path='/recommendations' element={<RecommendationsPage />} />
     </Routes>
+    <Footer/>
     </>
   )
 }
