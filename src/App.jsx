706cbf7ef61a703f25b65858635fe5d5ab7496da
EsmaelAbd <esmaelabd51@gmail.com>
2024-06-15 11:57:29 +0300

    added Place Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { Routes ,Route } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/HomePage/HomePage'
-import ExplorePage from './pages/ExplorePage/ExplorePage'
-import AboutPage from './pages/AboutPage/AboutPage'
-import BlogPage from './pages/BlogPage/BlogPage'
-import BookPage from './pages/BookPage/BookPage'
-import Navbar from './layout/Navbar/Navbar'
-import Sidebar from './layout/Sidebar/Sidebar'
-import RecommendationsPage from './pages/RecommendationsPage/RecommendationsPage'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage/Homepage";
+import ExplorePage from "./pages/ExplorePage/ExplorePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import BlogPage from "./pages/BlogPage/BlogPage";
+import BookPage from "./pages/BookPage/BookPage";
+import Navbar from "./layout/Navbar/Navbar";
+import Sidebar from "./layout/Sidebar/Sidebar";
+import RecommendationsPage from "./pages/RecommendationsPage/RecommendationsPage";
+import PlaceDetails from "./shared/PlaceDetails/PlaceDetails";
 function App() {
-
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/explore' element={<ExplorePage />} />
-      <Route path='/about' element={<AboutPage />} />
-      <Route path='/blog' element={<BlogPage />} />
-      <Route path='/booking' element={<BookPage />} />
-      <Route path='/recommendations' element={<RecommendationsPage />} />
-    </Routes>
+      {/* <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/booking" element={<BookPage />} />
+        <Route path="/recommendations" element={<RecommendationsPage />} />
+      </Routes> */}
+      <PlaceDetails />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
