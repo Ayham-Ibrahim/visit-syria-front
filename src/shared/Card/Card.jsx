973af1f7daa5d1adb6206695478c1aed326c3bb3
bushraYounes <boushra.ali.younes@gmail.com>
2024-06-18 00:31:30 +0300

    MainCard Updated

--- src/shared/Card/Card.jsx
+++ src/shared/Card/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 import { IoStarSharp } from "react-icons/io5";
 import { GrLocation } from "react-icons/gr";
 
-const Card = ({ image, title, rate, brief, location, button }) => {
+const Card = ({ image, title, rate, brief, location, button, price_exists, price, text }) => {
     return (
         <div className='BY_MainCard'>
             <div className='image_section'>
@@ -14,9 +14,18 @@ const Card = ({ image, title, rate, brief, location, button }) => {
                     <div className='title'>{title}</div>
                     <div className='rate'> {rate}<i><IoStarSharp /></i></div>
                 </div>
-                <div className="brief_section">{brief.slice(0, 185)} ...</div>
+                {price_exists ?
+                    <div className='middle_section'>
+                        <div className="location">{location}<i><GrLocation /></i></div>
+                        <div className="brief_section">{brief.slice(0, 185)} ...</div>
+                    </div> :
+                    <div className="brief_section">{brief.slice(0, 185)} ...</div>
+                }
                 <div className="bottom_section">
-                    <div className="location">{location}<i><GrLocation /></i></div>
+                    {price_exists ?
+                        <div className="price"> <span>  {price} $</span>{text}</div> :
+                        <div className="location">{location}<i><GrLocation /></i></div>
+                    }
                     <div className="button_section">{button}</div>
                 </div>
             </div>
